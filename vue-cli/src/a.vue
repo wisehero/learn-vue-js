e1d73ccc09d4fd003564a8c3852298a639797679
wisehero <godjw96@gmail.com>
2020-05-23 14:25:09 +0900

    Vue

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addNum: function() {}
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <form v-on:submit.prevent="submitForm" action="">
+    <div>
+      <label for="username">ID: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
